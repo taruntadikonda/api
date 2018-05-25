5ee91c25801b89f48cb27bb7e69aa4563f510e58
tarun <taditarun123@gmail.com>
2018-05-25 13:26:54 +0530

    workers done

--- index.js
+++ index.js
@@ -1,86 +1,19 @@
-var http = require('http');
-var https=require('https');
-var url = require('url');
-var fs=require('fs');
-var config=require('./lib/config');
-var data=require('./lib/data');
-var helpers=require('./lib/helpers');
-var handlers=require('./lib/handlers');
-var StringDecoder = require('string_decoder').StringDecoder;
+var server=require('./lib/server');
+var workers=require('./lib/workers');
 
-var httpserver = http.createServer(function(req,res){
-  common(req,res);
-});
+var lib=require('./lib/data');
 
-var httpsserveroptions={
-    'key':fs.readFileSync('./https/key.pem'),
-    'cert':fs.readFileSync('./https/cert.pem')
+
+var app={};
+
+
+app.init=function(){
+
+server.init();
+
+
+workers.init();
+
 }
 
-var httpsserver=https.createServer(httpsserveroptions,function(req,res)
-{
-    common(req,res);
-});
-
-
-
-httpserver.listen(config.httpport,function(){
-  console.log('The server '+config.httpport+" "+config.env);
-});
-
-httpsserver.listen(config.httpsport,function(){
-    console.log('The server '+config.httpsport+" "+config.env);
-});
-
-var common=function(req,res)
-{
-    var parsedUrl = url.parse(req.url, true);
-    var path = parsedUrl.pathname;
-    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-    var queryStringObject = parsedUrl.query;
-    var method = req.method;
-    var headers = req.headers;
-
-    var decoder = new StringDecoder('utf-8');
-    var buffer = '';
-    req.on('data', function(data) {
-        buffer += decoder.write(data);
-    });
-    req.on('end', function() {
-        buffer += decoder.end();
-
-
-        var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-        console.log(typeof(router[trimmedPath]));
-        var data = {
-          'trimmedPath' : trimmedPath,
-          'queryStringObject' : queryStringObject,
-          'method' : method,
-          'headers' : headers,
-          'payload' : helpers.parsejson(buffer)
-        };
-
-        chosenHandler(data,function(statusCode,payload){
-          statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-          payload = typeof(payload) == 'object'? payload : {};
-          var payloadString = JSON.stringify(payload);
-          res.setHeader('Content-Type','application/json');
-          res.writeHead(statusCode);
-          res.end(payloadString);
-          console.log("Returning this response: ",statusCode,payloadString);
-
-        });
-
-    });
-}
-
-
-
-var router = {
-  'tarun' : handlers.tarun,
-  'bhavana':handlers.bhavana,
-  'ping':handlers.ping,
-  'users':handlers.users,
-  'tokens':handlers.tokens,
-  'checks':handlers.checks
-};
+app.init();
